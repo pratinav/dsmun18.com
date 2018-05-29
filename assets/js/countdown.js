befc368f04fd35c5d4becf24f4d7792c6ad78d81
Shreyas Minocha <sm-git@riseup.net>
2018-05-29 19:30:20 +0530

    Add a countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+    const endDate = 'August 17, 2018 09:30:00 GMT+0530';
+
+    const daysSpan = document.getElementById('days');
+    const hoursSpan = document.getElementById('hours');
+    const minutesSpan = document.getElementById('minutes');
+    const secondsSpan = document.getElementById('seconds');
+
+    //////////////////////
+    // helper functions //
+    //////////////////////
+    function zeroPad(number) {
+        return ('0' + number).slice(-2);
+    }
+
+    function getRemainingTime(deadline) {
+        const t = Date.parse(deadline) - Date.parse(new Date());
+
+        if(t < 0) return {
+            'total': 0,
+            'days': 0,
+            'hours': 0,
+            'minutes': 0,
+            'seconds': 0
+        };
+
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    //////////////////
+    // actual stuff //
+    //////////////////
+    function initializeCountdown(deadline) {
+        function updateClock() {
+            const time = getRemainingTime(endDate);
+            const timeTag = document.getElementById('countdown');
+
+            timeTag.setAttribute('datetime', `P${time.days}DT${time.hours}H${time.minutes}M${time.seconds}S`);
+
+            daysSpan.innerHTML = zeroPad(time.days);
+            hoursSpan.innerHTML = zeroPad(time.hours);
+            minutesSpan.innerHTML = zeroPad(time.minutes);
+            secondsSpan.innerHTML = zeroPad(time.seconds);
+        }
+
+        updateClock(); // run function once at first to avoid delay
+        const timeInterval = setInterval(updateClock, 1000);
+    }
+
+    initializeCountdown('countdown', endDate);
+};
